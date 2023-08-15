48f1fbd90206b0c80ab29ca30320ae6facd70ee8
RunyShark <spartadragon117@live.com.mx>
2023-08-14 19:58:23 -0600

    Style Form

--- frontend/src/components/Organisms/FormTodo/index.tsx
+++ frontend/src/components/Organisms/FormTodo/index.tsx
@@ -1,5 +1,48 @@
-import React from 'react';
+import { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faFileSignature,
+  faSubtitles,
+} from '@fortawesome/pro-duotone-svg-icons';
+import { Todo } from '../../../store/slices/Todo/TodoSlice';
+import { Button } from '../..';
+
+type InitialFormSState = Partial<Todo>;
+
+const initialFormState: InitialFormSState = {
+  col: 'completed',
+  description: '',
+  title: '',
+};
 
 export const FormTodo = () => {
-  return <div>FormTodo</div>;
+  const [{ title, description }, setAddTodo] =
+    useState<InitialFormSState>(initialFormState);
+
+  return (
+    <form className="formTodo">
+      <div>
+        <input type="text" name={title} value={title} id="title" />
+        <label>title</label>
+        <FontAwesomeIcon icon={faFileSignature} />
+      </div>
+      <div>
+        <input
+          type="text"
+          name={description}
+          value={description}
+          id="description"
+        />
+        <label>description</label>
+        <FontAwesomeIcon icon={faSubtitles} />
+      </div>
+
+      <select name="status" id="status">
+        <option value="pending">Pending</option>
+        <option value="completed">Completed</option>
+        <option value="inProgress">In Progress</option>
+      </select>
+      <Button>Agregar</Button>
+    </form>
+  );
 };
